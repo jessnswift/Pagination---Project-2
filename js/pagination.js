f034185466c9f994c25e2b3fdd005bb6cf190708
Jessica Swift <jessica.nicole.swift@gmail.com>
2017-12-16 10:30:51 -0600

    itemsPerPage, show students, +example

--- js/pagination.js
+++ js/pagination.js
@@ -1,10 +1,24 @@
+const itemsPerPage = 10
 
-function showPage(pageNumber, studentList */) {
+function showPage(pageNumber, studentList) {
     // first hide all students on the page
     let list = studentList.children()
     list.hide()
 
     // Then loop through all students in our student list argument
+    let from = ((pageNumber - 1) * itemsPerPage) + 1;
+    let to = pageNumber * itemsPerPage;
+    for (i=0; i<=list.length; i++){
+
        // if student should be on this page number
-       	// show the student
+       if (i>=from && i<=to){
+
+        // show the student
+        $(list[i]).show()
+
+     }
+   };
  }
+
+let studentUl = $('.student-list');
+showPage(5, studentUl)
