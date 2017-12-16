2bf322017d1da6b623231584d2564faca8dd7065
Jessica Swift <jessica.nicole.swift@gmail.com>
2017-12-16 17:52:14 -0600

    appendpage links

--- js/pagination.js
+++ js/pagination.js
@@ -8,7 +8,8 @@ function showPage(pageNumber, studentList) {
     // Then loop through all students in our student list argument
     let from = ((pageNumber - 1) * itemsPerPage) + 1;
     let to = pageNumber * itemsPerPage;
-    for (i=0; i<=list.length; i++){
+    for (let i=0; i<=list.length; i++){
+      console.log(i);
 
        // if student should be on this page number
        if (i>=from && i<=to){
@@ -20,5 +21,54 @@ function showPage(pageNumber, studentList) {
    };
  }
 
-let studentUl = $('.student-list');
-showPage(5, studentUl)
+
+function appendPageLinks(studentList) {
+  let list = studentList.children()
+
+    // determine how many pages for this student list
+    let pageLinkCount = Math.ceil(list.length/itemsPerPage);
+
+    // create a page link section
+    let pageLinkHTML = `<ul class="pagination">`;
+
+    // “for” every page
+    for (let i=1; i<=pageLinkCount; i++){
+
+        // add a page link to the page link section
+        pageLinkHTML += `<li><a>${i}</a></li>`
+      }
+
+        // close the ul
+        pageLinkHTML += `</ul>`
+
+    // remove the old page link section from the site
+    $(".pagination").remove();
+
+    // append our new page link section to the site
+    $(".page").append(pageLinkHTML);
+
+    // define what happens when you click a link
+    let $pageLinks = $(".pagination a");
+    for (let i=0; i<=$pageLinks.length; i++) {
+      $($pageLinks[i]).on("click", function() {
+
+        // Use the showPage function to display the page for the link clicked
+        showPage(i+1, studentList)
+        removeActiveClass();
+
+        // mark that link as “active”
+        $($pageLinks[i]).addClass("active");
+    })
+  }
+}
+
+function removeActiveClass(){
+  let $pageLinks = $(".pagination a");
+  for (let i=0; i<=$pageLinks.length; i++) {
+    $($pageLinks[i]).removeClass("active");
+  }
+}
+
+appendPageLinks($(".student-list"));
+showPage(1, $(".student-list"));
+$(".pagination a").first().addClass("active");
